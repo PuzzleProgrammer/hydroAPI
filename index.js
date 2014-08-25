c550e8c12acb83a95efd75ad7420ccd9fcffaed8
Matt Andrews <matt@mattandre.ws>
2014-08-25 01:22:21 +0100

    Small whitespace typo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var Datastore = require('nedb');
 var todos = require('./lib/todos');
-var cors= require('cors');
+var cors = require('cors');
 var port = Number(process.env.PORT || 3000);
 
 var db = new Datastore({ filename: 'todos', autoload: true });
